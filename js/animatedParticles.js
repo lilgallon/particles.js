86ad690983a48b583a38d0b41a8771279dfb59f5
Lilian Gallon <lilian.gallon@etu.u-bordeaux.fr>
2019-01-27 20:37:56 +0100

    huge refactoring & remove useless functions

---
+++ js/animatedParticles.js
@@ -0,0 +1,177 @@
+/**
+ * GPL-3.0 License
+ * Author(s) : Lilian Gallon (N3ROO)
+ * Contribute here : https://github.com/N3ROO/Animated-Particles
+ * Troubleshooting : Instructions are available on the github page.
+ */
+
+class AnimatedParticles{
+
+    /**
+     * It creates a graph is the specified canvas
+     * @param canvas_id id of the canvas where we will draw (no modifications will be done on this canvas. You can even
+     * do something else on it, the particles will be drawn over it)
+     */
+    constructor(canvas_id){
+        this.canvas_id = canvas_id;
+        this.running = false;
+        this.starting = false;
+
+        this.DIST_TOLERANCE = 150;
+        this.STROKE_WIDTH = 3;
+
+        // Event handling
+        this.isMouseOver = false;
+        let self = this;
+        document.getElementById(this.canvas_id).addEventListener("mouseover", self.mouseOver.bind(this), false);
+        document.getElementById(this.canvas_id).addEventListener("mouseout", self.mouseOut.bind(this), false);
+    }
+
+    /**
+     * Start the graph (or resume it)
+     */
+    start(){
+        this.running = true;
+        this.starting = true;
+        this.run();
+    }
+
+    /**
+     * Stop the graph
+     */
+    stop(){
+        this.running = false;
+    }
+
+    /**
+     * This is the loop function
+     */
+    run(){
+        if(this.starting) {
+            this.init();
+            this.initParticleList(this.canvas.width / 20);
+            this.starting = false;
+        }
+
+        this.update();
+        this.draw();
+
+        if(this.running){
+            this.requestRedraw();
+        }
+    }
+
+    /**
+     *  Tells the browser that we wish to perform an animation and requests that the browser call a specified function
+     *  to update an animation before the next repaint.
+     */
+    requestRedraw(){
+        // We use a workaround to not lose the context (AnimatedParticle)
+        let self = this;
+        window.requestAnimationFrame(function () {self.run()});
+    }
+
+    /**
+     * Used to init all the variables used to run the graph
+     */
+    init(){
+        // We need to retrieve the canvas information
+        this.canvas = document.getElementById(this.canvas_id);
+        this.context = this.canvas.getContext("2d");
+
+        // We need a variable to store all the particles
+        this.particles = [];
+    }
+
+    /**
+     * Used to create particles of given amount
+     * @param amount of particles
+     */
+    initParticleList(amount){
+
+        for(let i = 0; i < amount; i++){
+
+            // We create the size first since we will use it to find the positions
+            let size = AnimatedParticles.random(2, 6);
+
+            // Position (make sure that it is not out of bounds)
+            let x = AnimatedParticles.random(size + 1, this.canvas.width - size);
+            let y = AnimatedParticles.random(size + 1, this.canvas.height - size);
+
+            // Then, we need the velocity vector (speed and direction)
+            let speed = AnimatedParticles.random(0.1, 1);
+            let direction = Math.random() * Math.PI * 2;
+
+            // Now we can create the particle
+            let dot = new Particle(x, y, size, speed, direction, 90);
+
+            // Add this dot to the particle list
+            this.particles.push(dot)
+        }
+    }
+
+    /**
+     * It updates all the particles (positions and springs)
+     */
+    update(){
+        for(let index in this.particles){
+            this.particles[index].update(this.canvas.width, this.canvas.height);
+        }
+    }
+
+    /**
+     * It draws all the particles
+     */
+    draw(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        for(let index in this.particles){
+            this.particles[index].draw(this.context);
+
+            // Now we need to draw the springs between particles
+            for(let neighbor_index in this.particles){
+                if(neighbor_index !== index){
+                    // Retrieve the distance
+                    let dist = this.particles[index].distanceTo(this.particles[neighbor_index]);
+
+                    if(dist < this.DIST_TOLERANCE){
+                        // Draw the spring
+                        this.context.beginPath();
+                        this.context.strokeWidth = this.STROKE_WIDTH;
+                        this.context.strokeStyle = "rgba(255,255,255," + (1 - dist/this.DIST_TOLERANCE) + ")";
+                        this.context.moveTo(this.particles[index].x, this.particles[index].y);
+                        this.context.lineTo(this.particles[neighbor_index].x, this.particles[neighbor_index].y);
+                        this.context.stroke()
+                    }
+                }
+            }
+        }
+    }
+
+    mouseOver(){
+        if(!this.isMouseOver) {
+            for (let index in this.particles) {
+                this.particles[index].setMultiplier(1.5);
+            }
+            this.isMouseOver = true;
+        }
+    }
+
+    mouseOut(){
+        if(this.isMouseOver) {
+            for (let index in this.particles) {
+                this.particles[index].setMultiplier(1);
+            }
+            this.isMouseOver = false;
+        }
+    }
+
+    /**
+     * Gives a random number between min and max [min; max[
+     * @param min
+     * @param max
+     */
+    static random(min, max){
+        return Math.random() * (max - min) + min;
+    }
+}
+
--- js/particle.js
+++ js/particle.js
@@ -2,115 +2,134 @@
  * GPL-3.0 License
  * Author(s) : Lilian Gallon (N3ROO)
  * Contribute here : https://github.com/N3ROO/Animated-Particles
+ * Troubleshooting : Instructions are available on the github page.
  */
 
-var particle={
-	x:0,
-	y:0,
-	vx:0,
-	vy:0,
-	mass:1,
-	radius:0,
-	friction:1,
-	gravity:0,
-	mass:1,
-	springs:null,
+class Particle{
 
-	create:function(x, y, speed, direction, grav){
-		// Constructor
-		var obj = Object.create(this);
-		obj.x = x;
-		obj.y = y;
-		obj.vx = Math.cos(direction)*speed;
-		obj.vy = Math.sin(direction)*speed;
-		obj.gravity = grav||0;
-		obj.springs = [];
-		return obj;
-	},
-	addSpring:function(point, k, length){
-		// Add a spring
-		this.removeSpring(point);
-		this.springs.push({
-			point:point,
-			k:k,
-			length:length||0,
-		});
-	},
-	removeSpring:function(point){
-		// Remove a spring
-		for(var i = 0; i < this.springs.length; i++){
-			if(point === this.springs[i].point){
-				this.springs.splice(i, 1);
-				return;
-			}
-		}
-	},
-	getSpeed:function(){
-		// Get the speed
-		return Math.sqrt(this.vx*this.vx + this.vy*this.vy);
-	},
-	setSpeed:function(speed){
-		// Set the speed
-		var heading = this.getHeading();
-		this.vx = Math.cos(heading)*speed;
-		this.vy = Math.sin(heading)*speed;
-	},
-	getHeading:function(){
-		// Get the heading (orientation)
-		return Math.atan2(this.vy, this.vx);
-	},
-	setHeading:function(heading){
-		// Set the heading
-		var speed = this.getSpeed();
-		this.vx = Math.cos(heading)*speed;
-		this.vy = Math.sin(heading)*speed;
-	},
-	accelerate:function(ax,ay){
-		// ACCELERATION
-		this.vx += ax;
-		this.vy += ay;
-	},
-	update:function(){
-		// Update the position
-		this.handleSprings();
-		this.vy += this.gravity;
-		this.vx *= this.friction,
-		this.vy *= this.friction,
-		this.x += this.vx;
-		this.y += this.vy;
-	},
-	handleSprings:function(){
-		// Update all the stuff that a spring needs to update
-		for(var i = 0; i < this.springs.length; i++){
-			var spring = this.springs[i];
-			this.springTo(spring.point, spring.k, spring.length);
-		}
-	},
-	angleTo:function(p2){
-		return Math.atan2(p2.y - this.y, p2.x - this.x);
-	},
-	distanceTo:function(p2){
-		var dx = p2.x - this.x,
-			dy = p2.y - this.y;
-		return Math.sqrt(dx*dx + dy*dy);
-	},
-	gravitateTo:function(p2){
-		var dx = p2.x - this.x,
-			dy = p2.y - this.y,
-			distSQ = dx*dx + dy*dy,
-			dist = Math.sqrt(distSQ),
-			force = p2.mass/distSQ,
-			ax = dx/dist * force,
-			ay = dy/dist * force;
-		this.vx += ax;
-		this.vy += ay;
-	},
-	springTo:function(point,k,length){
-		var dx = point.x - this.x,
-			dy = point.y - this.y,
-			distance = Math.sqrt(dx*dx + dy*dy),
-			springForce = (distance - length||0) * k;
-		this.vx += dx / distance * springForce;
-		this.vy += dy / distance * springForce;
-	}
+    /**
+     * Creates a particle
+     * @param x position on horizontal axis
+     * @param y position on vertical axis
+     * @param size the size of the particle
+     * @param speed the speed obviously
+     * @param direction in radians
+     * @param color hsl color
+     * @constructor
+     */
+    constructor(x, y, size, speed, direction, color){
+        this.x = x;
+        this.y = y;
+
+        this.size = size;
+
+        this.vx = Math.cos(direction) * speed;
+        this.vy = Math.sin(direction) * speed;
+
+        this.color = color;
+
+        this.multiplier = 1;
+
+        return this;
+    }
+
+    update(width, height){
+
+        let multiplier_increment = 0;
+        if(this.multiplier !== 1){
+            multiplier_increment = this.multiplier / 2;
+        }
+
+        this.setSpeed(this.getSpeed() + multiplier_increment);
+
+        // Change the position of the particles according to the borders
+        if(this.x + this.size > width || this.x - this.size < 0){
+            this.vx *= -1;
+            this.x = this.x + this.size > width ? width - this.size : this.size
+        }else{
+            this.x += this.vx;
+        }
+
+        if(this.y + this.size > height || this.y - this.size < 0){
+            this.vy *= - 1;
+            this.y = this.y + this.size > height ? height - this.size : this.size
+        }else{
+            this.y += this.vy;
+        }
+
+        this.setSpeed(this.getSpeed() - multiplier_increment);
+    }
+
+    draw(context){
+        context.beginPath();
+
+        // Figure
+        context.arc(this.x, this.y, this.getSize(), 0,Math.PI*2, false);
+
+        // Coloration
+        let brightness = 100;
+        if(this.multiplier > 1){
+            // TODO: check if it is okay (10: magic number)
+            brightness = 100 - (this.multiplier * 10);
+        }
+
+        context.fillStyle = 'hsl(' + this.color + ', 100%, ' + brightness + '%)';
+        context.fill();
+
+        context.closePath();
+    }
+
+    getSize(){
+        return this.size * this.multiplier;
+    }
+
+    /**
+     * @returns {number} : speed of the particle
+     */
+    getSpeed(){
+        return Math.sqrt(this.vx * this.vx + this.vy * this.vy);
+    }
+
+    /**
+     * Changes the speed of the particle
+     * @param speed new speed
+     */
+    setSpeed(speed){
+        // Set the speed
+        const direction = this.getDirection();
+        this.vx = Math.cos(direction) * speed;
+        this.vy = Math.sin(direction) * speed;
+    }
+
+    /**
+     * @returns {number} : the direction of the particle (radians)
+     */
+    getDirection(){
+        return Math.atan2(this.vy, this.vx);
+    }
+
+    /**
+     * Changes the direction of the particle
+     * @param direction new direction (radians)
+     */
+    setDirection(direction){
+        const speed = this.getSpeed();
+        this.vx = Math.cos(direction) * speed;
+        this.vy = Math.sin(direction) * speed;
+    }
+
+    setMultiplier(multiplier){
+        this.multiplier = multiplier;
+    }
+
+    /**
+     *
+     * @param particle
+     * @returns {number} : distance to the specified particle.
+     */
+    distanceTo(particle){
+        let dx = particle.x - this.x;
+        let dy = particle.y - this.y;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
 }
