9938b05c543484b4773eaf3792b0ace5d36f240d
Lilian Gallon <lilian.gallon@etu.u-bordeaux.fr>
2019-01-29 14:12:00 +0100

    change repo name, only one JS file, & add HOW TO doc

--- README.md
+++ README.md
@@ -1,43 +1,53 @@
 # particles.js
-![tag](https://img.shields.io/github/tag/n3roo/nero-engine.svg) ![issues](https://img.shields.io/github/issues/n3roo/nero-engine.svg) ![license](https://img.shields.io/github/license/n3roo/nero-engine.svg)
+![tag](https://img.shields.io/github/tag/n3roo/nero-engine.svg)
+![size](https://img.shields.io/github/size/:user/:repo/:path.svg)
+![issues](https://img.shields.io/github/issues/n3roo/nero-engine.svg)
+![license](https://img.shields.io/github/license/n3roo/nero-engine.svg)
 
-An easy way to make your website sexy
+An easy way to make your website sexy with particle backgrounds.
 
 ## Description
 
-I made this script to make my website look "modern". I decided to release the script to improve it from time to time. 
-At the moment, the script has a lot of design issues (there are some lines that are useless, some variables that aren't relevant, and so on). I will fix it as soon as possible.
+I made this script to make my website look "modern". I decided to release the script to improve it from time to time.
 
-Here is an example (or you can go to this [website](https://n3roo.github.io/)):
-![Example](git/example.png)
+Main features :
+- Highly customizable,
+- Easy to install,
+- Pure javascript (no dependencies),
+- Lightweight,
+- Compatible with all browsers.
 
 ## How to use it
 
-### What you should know
-
-If you use it, don't change the header of the javascript files. It should look like this :
-```javascript
-/**
- * GPL-3.0 License
- * Author(s) : Lilian Gallon (N3ROO), <and other contributors>, <you if you changed anything>
- * Contribute here : https://github.com/N3ROO/Animated-Particles
- */
+- Download the [lastest version](https://github.com/N3ROO/particles.js).
+- Include the script in your HTML.
+```html
+<script src="path/particles.js"></script>
+<!-- or -->
+<script src="path/particles.min.js"></script>
 ```
-However, if you put this code inside a javascript file that you made, make sure to put these informations somewhere, so it is visible.
 
-### How to finally use it
+- Add a canvas in a container where you want the particle background to be. The canvas will automatically be scaled with the parent container.
+```html
+<!-- Here, the canvas will fit the <div> container -->
+<div>
+<canvas id="canvas-id"></canvas>
+</div>
+```
 
-> TODO
+- Execute the script. Please see the next section to customize the particles background.
+```html
+<script>
+    (function() {
+        let particlesHandler = new ParticlesHandler("particles-canvas");
+        particlesHandler.start();
+    })();
+</script>
+```
 
-## Resource used
+## How to customize it
 
-- JQuery
-
-## TODO List
-
-- [ ] Improve particle class design
-- [ ] Improve portability (usable on any website without changing a lot of things)
-- [ ] Full JS implementation (no JQuery)
+> todo
 
 ## Contribution
 
--- js/particles.js
+++ js/particles.js
@@ -1,11 +1,11 @@
 /**
  * GPL-3.0 License
  * Author(s) : Lilian Gallon (N3ROO)
- * Contribute here : https://github.com/N3ROO/Animated-Particles
+ * Contribute here : https://github.com/N3ROO/particles.js
  * Troubleshooting : Instructions are available on the github page.
  */
 
-class AnimatedParticles{
+class ParticlesHandler{
 
     /**
      * It creates a graph is the specified canvas
@@ -66,7 +66,7 @@ class AnimatedParticles{
      *  to update an animation before the next repaint.
      */
     requestRedraw(){
-        // We use a workaround to not lose the context (AnimatedParticle)
+        // We use a workaround to not lose the context (ParticlesHandler)
         let self = this;
         window.requestAnimationFrame(function () {self.run()});
     }
@@ -102,17 +102,17 @@ class AnimatedParticles{
         for(let i = 0; i < amount; i++){
 
             // We create the size first since we will use it to find the positions
-            let size = AnimatedParticles.random(2, 6);
+            let size = ParticlesHandler.random(2, 6);
 
             // Position (make sure that it is not out of bounds)
-            let x = AnimatedParticles.random(size + 1, this.canvas.width - size);
-            let y = AnimatedParticles.random(size + 1, this.canvas.height - size);
+            let x = ParticlesHandler.random(size + 1, this.canvas.width - size);
+            let y = ParticlesHandler.random(size + 1, this.canvas.height - size);
 
             // Then, we need the velocity vector (speed and direction)
-            let speed = AnimatedParticles.random(0.1, 1);
+            let speed = ParticlesHandler.random(0.1, 1);
             let direction = Math.random() * Math.PI * 2;
 
-            let color = AnimatedParticles.random(0, 361);
+            let color = ParticlesHandler.random(0, 361);
 
             // Now we can create the particle
             let dot = new Particle(x, y, size, speed, direction, color);
@@ -192,3 +192,131 @@ class AnimatedParticles{
     }
 }
 
+class Particle{
+
+    /**
+     * Creates a particle
+     * @param x position on horizontal axis
+     * @param y position on vertical axis
+     * @param size the size of the particle
+     * @param speed the speed obviously
+     * @param direction in radians
+     * @param color hsl color
+     * @constructor
+     */
+    constructor(x, y, size, speed, direction, color){
+        this.x = x;
+        this.y = y;
+
+        this.size = size;
+
+        this.vx = Math.cos(direction) * speed;
+        this.vy = Math.sin(direction) * speed;
+
+        this.color = color;
+
+        this.multiplier = 1;
+
+        return this;
+    }
+
+    update(width, height){
+
+        let multiplier_increment = 0;
+        if(this.multiplier !== 1){
+            multiplier_increment = this.multiplier / 2;
+        }
+
+        this.setSpeed(this.getSpeed() + multiplier_increment);
+
+        // Change the position of the particles according to the borders
+        if(this.x + this.size > width || this.x - this.size < 0){
+            this.vx *= -1;
+            this.x = this.x + this.size > width ? width - this.size : this.size
+        }else{
+            this.x += this.vx;
+        }
+
+        if(this.y + this.size > height || this.y - this.size < 0){
+            this.vy *= - 1;
+            this.y = this.y + this.size > height ? height - this.size : this.size
+        }else{
+            this.y += this.vy;
+        }
+
+        this.setSpeed(this.getSpeed() - multiplier_increment);
+    }
+
+    draw(context){
+        context.beginPath();
+
+        // Figure
+        context.arc(this.x, this.y, this.getSize(), 0,Math.PI*2, false);
+
+        // Coloration
+        let brightness = 100;
+        if(this.multiplier > 1){
+            // TODO: check if it is okay (10: magic number)
+            brightness = 100 - (this.multiplier * 10);
+        }
+
+        context.fillStyle = 'hsl(' + this.color + ', 100%, ' + brightness + '%, 100%)';
+        context.fill();
+
+        context.closePath();
+    }
+
+    getSize(){
+        return this.size * this.multiplier;
+    }
+
+    /**
+     * @returns {number} : speed of the particle
+     */
+    getSpeed(){
+        return Math.sqrt(this.vx * this.vx + this.vy * this.vy);
+    }
+
+    /**
+     * Changes the speed of the particle
+     * @param speed new speed
+     */
+    setSpeed(speed){
+        // Set the speed
+        const direction = this.getDirection();
+        this.vx = Math.cos(direction) * speed;
+        this.vy = Math.sin(direction) * speed;
+    }
+
+    /**
+     * @returns {number} : the direction of the particle (radians)
+     */
+    getDirection(){
+        return Math.atan2(this.vy, this.vx);
+    }
+
+    /**
+     * Changes the direction of the particle
+     * @param direction new direction (radians)
+     */
+    setDirection(direction){
+        const speed = this.getSpeed();
+        this.vx = Math.cos(direction) * speed;
+        this.vy = Math.sin(direction) * speed;
+    }
+
+    setMultiplier(multiplier){
+        this.multiplier = multiplier;
+    }
+
+    /**
+     *
+     * @param particle
+     * @returns {number} : distance to the specified particle.
+     */
+    distanceTo(particle){
+        let dx = particle.x - this.x;
+        let dy = particle.y - this.y;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+}
