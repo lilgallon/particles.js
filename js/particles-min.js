286f4eb94add5c1745b8c935e18811345490de13
Lilian Gallon <swellchaser33@gmail.com>
2019-02-19 13:50:16 +0100

    update version number in headers

--- js/particles-min.js
+++ js/particles-min.js
@@ -1,6 +1,6 @@
 /**
  * particles.js
- * Current version : 1.2
+ * Current version : 1.3
  * Author(s) : Lilian Gallon (N3ROO)
  * Troubleshooting : Instructions are available on the github page.
  * Contribute here : https://github.com/N3ROO/particles.js
