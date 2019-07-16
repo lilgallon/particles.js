076c0af9d9749243a9771774abc3b4e83ba02cc7
nero <swellchaser33@gmail.com>
2019-07-16 17:43:58 +0200

    fix #20 : methods that should have been private are now private

--- js/particles.js
+++ js/particles.js
@@ -47,7 +47,7 @@
     start(){
         this._running = true;
         this._starting = true;
-        this.run();
+        this._run();
     }
 
     /**
@@ -60,14 +60,14 @@
     /**
      * This is the loop function.
      */
-    run(){
+    _run(){
         if(this._starting) {
             if(this._verbose){
                 console.log("First start, we need to init everything.");
             }
 
-            this.init();
-            this.initParticleList();
+            this._init();
+            this._initParticleList();
             this._starting = false;
 
             if(this._verbose && this._running){
@@ -79,10 +79,10 @@
 
         // It could have been cancelled during init if an error occurred
         if(this._running){
-            this.update();
-            this.draw();
+            this._update();
+            this._draw();
 
-            this.requestRedraw();
+            this._requestRe_draw();
         }else{
             if(this._verbose){
                 console.log("Loop stopped.");
@@ -94,16 +94,16 @@
      * Tells the browser that we wish to perform an animation and requests that the browser call a specified function
      * to update an animation before the next repaint.
      */
-    requestRedraw(){
+    _requestRe_draw(){
         // We use a workaround to not lose the context (ParticlesHandler)
         let self = this;
-        window.requestAnimationFrame(function () {self.run()});
+        window.requestAnimationFrame(function () {self._run()});
     }
 
     /**
      * Used to init all the variables used to run the graph.
      */
-    init(){
+    _init(){
         if(this._verbose){
             console.log("particlesHandler initialization.");
         }
@@ -151,11 +151,11 @@
         // Now we can get the information
         this._context = this._canvas.getContext("2d");
 
-        this.loadSettings(this._settings);
+        this._loadSettings(this._settings);
 
         // Check if it is disabled on mobile and if the user uses a mobile
         if(this._settings.disableOnMobile === 1){
-            if(this.isOnMobile()){
+            if(this._isOnMobile()){
                 if(this._verbose){
                     console.log("The particles script is disabled for mobile users.")
                 }
@@ -166,8 +166,8 @@
 
         // Set up mouse event listeners
         let self = this;
-        this._canvas.parentElement.addEventListener("mouseover", self.mouseOver.bind(this), false);
-        this._canvas.parentElement.addEventListener("mouseout", self.mouseOut.bind(this), false);
+        this._canvas.parentElement.addEventListener("mouseover", self._mouseOver.bind(this), false);
+        this._canvas.parentElement.addEventListener("mouseout", self._mouseOut.bind(this), false);
 
         // Resize event listener
         window.addEventListener('resize', self.onResize.bind(this));
@@ -177,7 +177,7 @@
      * It verifies and loads the settings.
      * @param {dict} settings variable with all the settings (check github for details)
      */
-    loadSettings(settings){
+    _loadSettings(settings){
         if(this._verbose){
             console.log("Loading settings.");
         }
@@ -215,42 +215,42 @@
             }
         };
 
-        this.loadSetting(settings, "disableOnMobile", 1, 0, 1);
+        this._loadSetting(settings, "disableOnMobile", 1, 0, 1);
 
         let amount = this._canvas.width * this._canvas.height / 7000
-        this.loadSetting(settings, "amount"       , amount, 0, Number.MAX_SAFE_INTEGER);
-        this.loadSetting(settings, "amountMin"    , amount - 0.5 * amount, 0, Number.MAX_SAFE_INTEGER);
-        this.loadSetting(settings, "amountMax"    , amount + 0.2 * amount, 0, Number.MAX_SAFE_INTEGER);
-        this.loadSetting(settings, "dynamicAmount", 1, 0, 1);
+        this._loadSetting(settings, "amount"       , amount, 0, Number.MAX_SAFE_INTEGER);
+        this._loadSetting(settings, "amountMin"    , amount - 0.5 * amount, 0, Number.MAX_SAFE_INTEGER);
+        this._loadSetting(settings, "amountMax"    , amount + 0.2 * amount, 0, Number.MAX_SAFE_INTEGER);
+        this._loadSetting(settings, "dynamicAmount", 1, 0, 1);
 
-        this.loadSetting(settings, "tolerance"    , 150, 0, Number.MAX_SAFE_INTEGER);
-        this.loadSetting(settings, "lineWidth"    , 3  , 0, Number.MAX_SAFE_INTEGER);
+        this._loadSetting(settings, "tolerance"    , 150, 0, Number.MAX_SAFE_INTEGER);
+        this._loadSetting(settings, "lineWidth"    , 3  , 0, Number.MAX_SAFE_INTEGER);
 
-        this.loadSetting(settings, "sizeMin"     , 2, 0, Number.MAX_SAFE_INTEGER);
-        this.loadSetting(settings, "sizeMax"     , 6, 0, Number.MAX_SAFE_INTEGER);
+        this._loadSetting(settings, "sizeMin"     , 2, 0, Number.MAX_SAFE_INTEGER);
+        this._loadSetting(settings, "sizeMax"     , 6, 0, Number.MAX_SAFE_INTEGER);
 
-        this.loadSetting(settings, "positionXMin", settings.sizeMax + 1, settings.sizeMax + 1, this._canvas.width - settings.sizeMax - 1);
-        this.loadSetting(settings, "positionXMax", this._canvas.width - settings.sizeMax, settings.sizeMax + 1, this._canvas.width - settings.sizeMax - 1);
-        this.loadSetting(settings, "positionYMin", settings.sizeMax + 1, settings.sizeMax + 1, this._canvas.height - settings.sizeMax - 1);
-        this.loadSetting(settings, "positionYMax", this._canvas.height - settings.sizeMax, settings.sizeMax + 1, this._canvas.height - settings.sizeMax - 1);
-        this.loadSetting(settings, "speedMin"    , 200, 0, Number.MAX_SAFE_INTEGER);
-        this.loadSetting(settings, "speedMax"    , 400, 0, Number.MAX_SAFE_INTEGER);
-        this.loadSetting(settings, "directionMin", 0, 0, Math.PI * 2);
-        this.loadSetting(settings, "directionMax", Math.PI * 2, 0, Math.PI * 2);
-        this.loadSetting(settings, "colorMin"    , 0, 0, 360);
-        this.loadSetting(settings, "colorMax"    , 360, 0, 360);
+        this._loadSetting(settings, "positionXMin", settings.sizeMax + 1, settings.sizeMax + 1, this._canvas.width - settings.sizeMax - 1);
+        this._loadSetting(settings, "positionXMax", this._canvas.width - settings.sizeMax, settings.sizeMax + 1, this._canvas.width - settings.sizeMax - 1);
+        this._loadSetting(settings, "positionYMin", settings.sizeMax + 1, settings.sizeMax + 1, this._canvas.height - settings.sizeMax - 1);
+        this._loadSetting(settings, "positionYMax", this._canvas.height - settings.sizeMax, settings.sizeMax + 1, this._canvas.height - settings.sizeMax - 1);
+        this._loadSetting(settings, "speedMin"    , 200, 0, Number.MAX_SAFE_INTEGER);
+        this._loadSetting(settings, "speedMax"    , 400, 0, Number.MAX_SAFE_INTEGER);
+        this._loadSetting(settings, "directionMin", 0, 0, Math.PI * 2);
+        this._loadSetting(settings, "directionMax", Math.PI * 2, 0, Math.PI * 2);
+        this._loadSetting(settings, "colorMin"    , 0, 0, 360);
+        this._loadSetting(settings, "colorMax"    , 360, 0, 360);
 
-        this.loadSetting(settings, "multiplierIn" , 1.5, 0.001, Number.MAX_SAFE_INTEGER);
-        this.loadSetting(settings, "multiplierOut", 1, 0.001, Number.MAX_SAFE_INTEGER);
+        this._loadSetting(settings, "multiplierIn" , 1.5, 0.001, Number.MAX_SAFE_INTEGER);
+        this._loadSetting(settings, "multiplierOut", 1, 0.001, Number.MAX_SAFE_INTEGER);
 
-        this.loadSetting(settings, "springColorR", 255, 0, 255);
-        this.loadSetting(settings, "springColorG", 255, 0, 255);
-        this.loadSetting(settings, "springColorB", 255, 0, 255);
+        this._loadSetting(settings, "springColorR", 255, 0, 255);
+        this._loadSetting(settings, "springColorG", 255, 0, 255);
+        this._loadSetting(settings, "springColorB", 255, 0, 255);
 
         this._settings = settings;
     }
 
-    loadSetting(settings, settingName, defaultValue, minValue, maxValue){
+    _loadSetting(settings, settingName, defaultValue, minValue, maxValue){
         let status;
         if(settings[settingName] === undefined){
             settings[settingName] = defaultValue;
@@ -276,7 +276,7 @@
      * Credits : http://detectmobilebrowsers.com/
      * @returns whether or not the user is on a mobile.
      */
-    isOnMobile(){
+    _isOnMobile(){
         var check = false;
         (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
         return check;
@@ -285,7 +285,7 @@
     /**
      * Used to create particles.
      */
-    initParticleList(){
+    _initParticleList(){
 
         if(this._verbose){
             console.log("Creating " + this._settings.amount + " particles.");
@@ -298,26 +298,26 @@
                 this._settings.amountMin : this._settings.amount);
 
         for(let i = 0; i < amount; i++){
-            this._particles.push(this.createParticleWithCurrentSettings());
+            this._particles.push(this._createParticleWithCurrentSettings());
         }
     }
 
     /**
      * @returns a particle created according to the current _settings.
      */
-    createParticleWithCurrentSettings(){
+    _createParticleWithCurrentSettings(){
         // We create the size first since we will use it to find the positions
-        let size = ParticlesHandler.random(this._settings.sizeMin, this._settings.sizeMax);
+        let size = ParticlesHandler._random(this._settings.sizeMin, this._settings.sizeMax);
 
         // Position (make sure that it is not out of bounds)
-        let x = ParticlesHandler.random(this._settings.positionXMin, this._settings.positionXMax);
-        let y = ParticlesHandler.random(this._settings.positionYMin, this._settings.positionYMax);
+        let x = ParticlesHandler._random(this._settings.positionXMin, this._settings.positionXMax);
+        let y = ParticlesHandler._random(this._settings.positionYMin, this._settings.positionYMax);
 
         // Then, we need the velocity vector (speed and direction)
-        let speed = ParticlesHandler.random(this._settings.speedMin, this._settings.speedMax);
-        let direction = ParticlesHandler.random(this._settings.directionMin, this._settings.directionMax);
+        let speed = ParticlesHandler._random(this._settings.speedMin, this._settings.speedMax);
+        let direction = ParticlesHandler._random(this._settings.directionMin, this._settings.directionMax);
 
-        let color = ParticlesHandler.random(this._settings.colorMin, this._settings.colorMax);
+        let color = ParticlesHandler._random(this._settings.colorMin, this._settings.colorMax);
 
         // Now we can create the particle
         return new Particle(x, y, size, speed / 1000, direction, color);
@@ -326,7 +326,7 @@
     /**
      * It updates all the particles (positions and springs).
      */
-    update(){
+    _update(){
         for(let index in this._particles){
             this._particles[index].update(this._canvas.width, this._canvas.height);
         }
@@ -335,7 +335,7 @@
     /**
      * It draws all the particles.
      */
-    draw(){
+    _draw(){
         this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
         for(let index in this._particles){
             // We need to draw the springs between particles before them otherwise, the springs
@@ -373,7 +373,7 @@
     /**
      * Called when the mouse is over the canvas.
      */
-    mouseOver(){
+    _mouseOver(){
         // We make sure that we apply the multiplier only once, and not
         // at every tick when the mouse is over.
         if(!this._isMouseOver) {
@@ -391,7 +391,7 @@
     /**
      * Called when the mouse is out of the canvas.
      */
-    mouseOut(){
+    _mouseOut(){
         // We make sure that we apply the multiplier only once, and not
         // at every tick when the mouse is out.
         if(this._isMouseOver) {
@@ -435,15 +435,15 @@
         }
 
         // Recreate particles list
-        this.initParticleList();
+        this._initParticleList();
     }
 
     /**
-     * Gives a random number between min and max [min; max].
+     * Gives a _random number between min and max [min; max].
      * @param min,
      * @param max.
      */
-    static random(min, max){
+    static _random(min, max){
         return Math.random() * ((max + 1) - min) + min;
     }
 
@@ -482,7 +482,7 @@
      */
     set settings(settings){
         // disableOnMobile: stop the loop if needed
-        if(settings.disableOnMobile && this.isOnMobile() && this._running){
+        if(settings.disableOnMobile && this._isOnMobile() && this._running){
             this.stop();
             this._particles = [];
         }
@@ -520,7 +520,7 @@
             }
 
             for(let i = 0; i < amountToAdd; i++){
-                this._particles.push(this.createParticleWithCurrentSettings());
+                this._particles.push(this._createParticleWithCurrentSettings());
             }
         }
 
@@ -562,34 +562,34 @@
         // springColorB: nothing special to do
 
         // It validates and apply the settings
-        this.loadSettings(settings);
+        this._loadSettings(settings);
 
         if(sizeUpdated || positionUpdated || speedUpdated || colorUpdated){
             for(let index in this._particles){
                 if(sizeUpdated){
                     if(this._particles[index].size < this._settings.sizeMin || this._particles[index].size > this._settings.sizeMax){
-                        this._particles[index].size = ParticlesHandler.random(this._settings.sizeMin, this._settings.sizeMax);
+                        this._particles[index].size = ParticlesHandler._random(this._settings.sizeMin, this._settings.sizeMax);
                     }
                 }
 
                 if(positionUpdated){
                     if(this._particles[index].x < this._settings.positionXMin || this._particles[index].x > this._settings.positionXMax){
-                        this._particles[index].x = ParticlesHandler.random(this._settings.positionXMin, this._settings.positionXMax);
+                        this._particles[index].x = ParticlesHandler._random(this._settings.positionXMin, this._settings.positionXMax);
                     }
                     if(this._particles[index].y < this._settings.positionYMin || this._particles[index].y > this._settings.positionYMax){
-                        this._particles[index].y = ParticlesHandler.random(this._settings.positionYMin, this._settings.positionYMax);
+                        this._particles[index].y = ParticlesHandler._random(this._settings.positionYMin, this._settings.positionYMax);
                     }
                 }
 
                 if(speedUpdated){
                     if(this._particles[index].getSpeed() < this._settings.speedMin || this._particles[index].getSpeed() > this._settings.speedMax){
-                        this._particles[index].setSpeed(ParticlesHandler.random(this._settings.speedMin, this._settings.speedMax));
+                        this._particles[index].setSpeed(ParticlesHandler._random(this._settings.speedMin, this._settings.speedMax));
                     }
                 }
 
                 if(colorUpdated){
                     if(this._particles[index].color < this._settings.colorMin || this._particles[index].color > this._settings.colorMax){
-                        this._particles[index].color = ParticlesHandler.random(this._settings.colorMin, this._settings.colorMax);
+                        this._particles[index].color = ParticlesHandler._random(this._settings.colorMin, this._settings.colorMax);
                     }
                 }
             }
